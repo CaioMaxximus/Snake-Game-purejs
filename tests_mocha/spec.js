1e4278d2b6ddea460bbb133dcc99227fb6b40857
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2021-11-18 23:34:43 -0300

    adding_test_ambient_config

--- tests_mocha/spec.js
+++ tests_mocha/spec.js
@@ -1,16 +1,19 @@
-import {pixel} from '../src/PixelObserver.js'
-
+import {pixel} from "../src/PixelObserver.js"
 
 describe("Testando pixelObserver" , function(){
 
 
     let pix = new pixel('00', "green");
+    // console.log(p);
+    // console.log("it");  
 
     it("Testando getters", function (){
         expect(pix.getId()).to.be.equal('00');
         console.log("pass")
-        expect(pix.getColor).to.be.equal("green");
-    })
+        expect(pix.getColor()).to.be.equal("green");
+    });
+   
+
 
 
 });
